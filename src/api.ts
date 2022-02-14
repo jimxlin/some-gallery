6a939cbe10a47088b8a52488b8a7df421c355d6e
Jim X. Lin <miljinx@gmail.com>
2022-02-13 20:23:04 -0800

    Get S3 object tags

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,7 @@ import {
   S3Client,
   ListObjectsV2Command,
   GetObjectCommand,
+  GetObjectTaggingCommand,
 } from "@aws-sdk/client-s3";
 
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
@@ -21,6 +22,7 @@ export const getPhotoSrc = async (Key: string) => {
       Key,
     })
   );
+  // SDKv3 returns S3 object as ReadableStream
   // https://github.com/aws/aws-sdk-js-v3/issues/1877#issuecomment-986481764
   // https://transang.me/modern-fetch-and-how-to-get-buffer-output-from-aws-sdk-v3-getobjectcommand/
   const resp = new Response(file.Body as Blob);
@@ -28,9 +30,30 @@ export const getPhotoSrc = async (Key: string) => {
   return URL.createObjectURL(blob);
 };
 
+const getPhotoTag = async (key: string) => {
+  const { TagSet } = await s3Client.send(
+    new GetObjectTaggingCommand({
+      Bucket: process.env.REACT_APP_AWS_ACCESS_POINT,
+      Key: key,
+    })
+  );
+  return TagSet?.reduce((tags, tag) => {
+    if (!tag.Key) return tags;
+    return { ...tags, [tag.Key]: tag.Value };
+  }, {});
+};
+
 export const getPhotoList = async () => {
   const { Contents } = await s3Client.send(
     new ListObjectsV2Command({ Bucket: process.env.REACT_APP_AWS_ACCESS_POINT })
   );
-  return Contents;
+  const contentWithTags = await Promise.all(
+    (Contents || []).map(async (obj) => {
+      if (!obj.Key) return obj;
+      const photoTags = await getPhotoTag(obj.Key);
+      if (!photoTags) return obj;
+      return { ...obj, tags: photoTags };
+    })
+  );
+  return contentWithTags;
 };
