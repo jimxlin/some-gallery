d248aa46ee0541b817103606187abecce229be37
Jim X. Lin <miljinx@gmail.com>
2022-02-19 15:04:08 -0800

    Update photo frame sizing

--- src/components/PhotoFrame.tsx
+++ src/components/PhotoFrame.tsx
@@ -1,27 +1,29 @@
-import { Square, Image } from "@chakra-ui/react";
-import { FRAME_HEIGHTS } from "../constants";
+import { Center, Image } from "@chakra-ui/react";
+import { FRAME_HEIGHTS, FRAME_PADDINGS } from "../constants";
 
 type Props = {
   imageSrc: string;
 };
 function PhotoFrame({ imageSrc }: Props) {
   return (
-    <Square
+    <Center
       onContextMenu={(e) => e.preventDefault()}
       bgImage="/photoframe-light.png"
       bgPosition="center"
       bgRepeat="repeat"
-      size={FRAME_HEIGHTS} // avoid percentages
+      h={FRAME_HEIGHTS} // avoid percentages
+      p={FRAME_PADDINGS}
       boxShadow="outsetLight"
       rounded="xs"
     >
       <Image
-        h="70%"
+        maxW="100%"
+        maxH="100%"
         boxShadow="insetLight"
         border="1px solid rgb(0, 0, 0, 0.1)"
         src={imageSrc}
       />
-    </Square>
+    </Center>
   );
 }
 export default PhotoFrame;
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,3 @@
-export const FRAME_HEIGHTS = ["16rem", "24rem", "36rem"];
+export const FRAME_HEIGHTS = ["16rem", "24rem", "32rem"];
+export const FRAME_PADDINGS = ["2rem", "3rem", "4rem"];
 export const ADJECTIVES_TAG = "adjectives";
