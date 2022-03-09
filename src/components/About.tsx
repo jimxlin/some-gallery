2d40b2e4c129f111fb803a339e6739ad1067c86b
Jim X. Lin <miljinx@gmail.com>
2022-03-08 23:05:53 -0800

    Update About content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,8 +4,11 @@ import { FaLongArrowAltDown } from "react-icons/fa";
 function About() {
   return (
     <VStack w={["90%", "90%", "60%"]} spacing={[2, 4]} align="start">
-      <Text>Photography is a hobby, web development is my profession.</Text>
-      <Text>Please enjoy some of my photographs shown here.</Text>
+      <Text>
+        Hi, I'm a hobby photographer. I like the process of analog as well as
+        the convenience of digital.
+      </Text>
+      <Text>I hope you enjoy some of my photographs shown here.</Text>
       <Center w="100%">
         <Icon as={FaLongArrowAltDown} w={4} h={36} />
       </Center>
